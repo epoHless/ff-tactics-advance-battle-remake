f58e8e6cc3816b0bfeecf19e3fc71849cdb43b4d
Kevin Salimbeni <73953277+epoHless@users.noreply.github.com>
2023-04-17 01:12:09 +0200

    Added menu

---
+++ ff-tactics-advance-remake/Assets/_Scripts/UI/Player Menu/Buttons/Menu Buttons/Implemented Buttons/LoadingScreen.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class LoadingScreen : Singleton<LoadingScreen>
+{
+    [SerializeField] private Image loadingBar;
+    [SerializeField] private CanvasGroup canvasGroup;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    private void Start()
+    {
+        canvasGroup.alpha = 0;
+        canvasGroup.interactable = false;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void LoadScreen()
+    {
+        LeanTween.value(gameObject, f => { canvasGroup.alpha = f; }, 0, 1, .5f).setOnComplete(() =>
+        {
+            LeanTween.value(gameObject, f => { loadingBar.fillAmount = f; }, 0, 1, 5f).setOnComplete((() =>
+            {
+                SceneManager.LoadSceneAsync(1).completed += operation =>
+                {
+                    LeanTween.value(gameObject, f => { canvasGroup.alpha = f; }, 1, 0, .5f).setOnComplete(() => gameObject.SetActive(false));
+                };
+            }));
+        });
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
